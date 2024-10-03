cc7d6ac09fdba5cf416d10c218f4d18cf2cdbe0c
.xyz <37243611+PowerShop@users.noreply.github.com>
2024-10-03 15:31:56 +0700

    Add files via upload

---
+++ examples/app.js
@@ -0,0 +1,10 @@
+function doGet(e) {
+    // Create your attribute for receiving data
+    var text = e.parameter.text;
+
+    // Call the function to send data to Google Sheets
+    var urls="https://docs.google.com/spreadsheets/d/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/edit?gid=0";
+    var ss=SpreadsheetApp.openByUrl(urls);
+    var ws=ss.getSheetByName("YOUR_SHEET_NAME");
+    ws.appendRow([new Date(),text]);
+}
